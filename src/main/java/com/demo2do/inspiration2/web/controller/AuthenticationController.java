b045b36543ccfabdf76622c80fbc13b75b0f9eb2
Ziyuan Liu <lutepluto@gmail.com>
2014-03-18 22:54:05 +0800

    add timeout checking and error message display in login.jsp

--- src/main/java/com/demo2do/inspiration2/web/controller/AuthenticationController.java
+++ src/main/java/com/demo2do/inspiration2/web/controller/AuthenticationController.java
@@ -3,12 +3,15 @@ package com.demo2do.inspiration2.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("")
@@ -17,22 +20,51 @@ public class AuthenticationController {
 	private static final Log logger = LogFactory.getLog(AuthenticationController.class);
 	
 	/**
+	 * session invalid redirect
+	 * 
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@RequestMapping("/timeout")
+	public String timeout(RedirectAttributes redirectAttributes) {
+		
+		logger.info("Session timeout... The system will redirect to login page.");
+		
+		// add a flag into session to display on login page
+		redirectAttributes.addFlashAttribute("timeoutErrorMessage", "security.authentication.timeout");
+		
+		return "redirect:/login";
+	}
+	
+	/**
 	 * @param request
 	 * @param httpSession
 	 * @return
 	 */
 	@RequestMapping(value = {"/", "/login"})
-	public String login(HttpServletRequest request, HttpSession httpSession) {
+	public String login(HttpServletRequest request, HttpSession httpSession, @ModelAttribute("timeoutErrorMessage") String timeoutErrorMessage) {
 		
 		AuthenticationException authenticationException = 
 				(AuthenticationException) httpSession.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
 		
+		// when authentication exception is found, setting up error message to display
 		if (authenticationException != null) {
 			
 			if(logger.isErrorEnabled()) {
 				logger.error("Authentication process exception: " + authenticationException.getMessage());
 			}
 			
+			request.setAttribute("errorMessage", "security.authentication.fail");
+			
+			// remove AuthenticationException from httpSession
+			httpSession.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+			
+			return "login";
+		}
+		
+		if (StringUtils.isNotEmpty(timeoutErrorMessage)) {
+			
+			request.setAttribute("errorMessage", timeoutErrorMessage);
 		}
 		
 		return "login";
@@ -55,5 +87,5 @@ public class AuthenticationController {
 		
 		return "redirect: /login";
 	}
-
+	
 }
--- webapp/WEB-INF/jsps/login.jsp
+++ webapp/WEB-INF/jsps/login.jsp
@@ -1,13 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html>
 <html lang="zh-CN">
   <head>
+  	
+  	<fmt:setBundle basename="ApplicationMessage" />
+  	<fmt:setLocale value="zh_CN"/>
+  
     <meta charset="utf-8">
     <meta name="viewpoint" content="width=device-width, initial-scale=1">
     <title>Login - Inspiration</title>
   </head>
   <body>
+  
+  	<c:if test="${not empty errorMessage}">
+  		<fmt:message key="${errorMessage}" />
+  	</c:if>
+  	
     <form action="<c:url value='j_spring_security_check' />" method="POST">
               用户名：<input type="text" name="j_username">
 	  密　码：<input type="password" name="j_password">
